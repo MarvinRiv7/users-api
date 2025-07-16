9e5a5d79aefe14fa9f5584f97a3724aea3cc4cdd
Marvin Rivas <mr2444749@gmail.com>
2025-07-16 09:43:52 -0600

    MMMM

--- types/express/index.d.ts
+++ types/express/index.d.ts
@@ -5,6 +5,6 @@ import { Document } from 'mongoose';
 declare module 'express-serve-static-core' {
   interface Request {
     uid?: string;
-    usuario: Document<any, any, IUsuarioo> & IUsuario;
+    usuario: Document<any, any, IUsuario> & IUsuario;
   }
 }
